91e3473d80689482f04b575d8d79ab2bf02e6930
Вадим <glazkow.14.vadim@yandex.u>
2020-11-25 20:35:09 +0300

    Рисование желтых кругов по нажатию кнопки

--- draw.py
+++ draw.py
@@ -1,15 +1,15 @@
 import sys
 
 from random import randint
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from button import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('button.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.count)
         self.draw_circles = False
 
@@ -28,16 +28,16 @@ class MyWidget(QMainWindow):
             self.draw_circles = False
 
     def draw_flag(self, qp):
-        qp.setPen(QColor(255, 255, 0))
         score = randint(5, 10)
         for _ in range(score):
+            r_color = randint(0, 255)
+            g_color = randint(0, 255)
+            b_color = randint(0, 255)
+            qp.setPen(QColor(r_color, g_color, b_color))
             diameter = randint(50, 100)
             x = randint(diameter, 660 - diameter)
             y = randint(diameter, 325 - diameter)
             qp.drawEllipse(x, y, diameter, diameter)
-            print(x, y, diameter)
-
-
 
     def except_hook(cls, exception, traceback):
             sys.__excepthook__(cls, exception, traceback)
